a63ad0a0190587cbebfd79c2f653eb0b06915caa
kchangju <125858634+kchangju@users.noreply.github.com>
2023-03-21 19:45:24 +0900

    fix: [김창주] FAQ 게시판 코드설명 추가 [SSS-PROCESS-8]
    fix: [김창주] FAQ 게시판 코드설명 추가 [SSS-PROCESS-8]

--- backend/src/main/java/com/example/demo/domain/board/service/BoardService.java
+++ backend/src/main/java/com/example/demo/domain/board/service/BoardService.java
@@ -6,20 +6,39 @@ import com.example.demo.domain.board.entity.Board;
 import java.util.List;
 
 public interface BoardService {
-    //public void register(BoardRequest boardRequest);
+
+    /**
+     * 게시물 저장
+     */
     public Board register(BoardRequest boardRequest);
 
+    /**
+     * 게시물 리스트
+     */
     List<Board> list();
 
+    /**
+     * 게시물 읽기
+     */
     Board read(Long boardId);
 
+    /**
+     * 게시물 삭제
+     */
     void remove(Long boardId);
 
+    /**
+     * 게시물 수정
+     */
     Board modify(Long boardId, BoardRequest boardRequest);
 
-    List<Board> bigMisstake(Long boardId, BoardRequest boardRequest);
-
+    /**
+     * 게시물 개수
+     */
     Long getCount();
 
+    /**
+     * 마지막 엔티티 id번호
+     */
     Long getLastEntityId();
 }
