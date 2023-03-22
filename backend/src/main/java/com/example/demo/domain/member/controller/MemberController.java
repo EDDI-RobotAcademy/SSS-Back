41e26d9d7f3fd6cf242c7150f214da5a1f430771
dduru-nuna <syj00062@gmail.com>
2023-03-23 01:30:04 +0900

    fix: [서윤지] 맵핑 설정 누락 수정 [SSS-PROCESS-19]

--- backend/src/main/java/com/example/demo/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/example/demo/domain/member/controller/MemberController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("/member")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class MemberController {
