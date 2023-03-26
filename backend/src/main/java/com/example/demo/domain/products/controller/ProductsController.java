abc4da0121d63732715c133ee8f389782e2b48ae
dduru-nuna <syj00062@gmail.com>
2023-03-26 17:26:53 +0900

    feat: [서윤지] 상품 게시판에 등록된 상품 목록을 전달하는 시스템[SSS-PROCESS-21]

--- backend/src/main/java/com/example/demo/domain/products/controller/ProductsController.java
+++ backend/src/main/java/com/example/demo/domain/products/controller/ProductsController.java
@@ -1,29 +1,35 @@
 package com.example.demo.domain.products.controller;
 
+import com.example.demo.domain.products.controller.form.ProductsRegisterForm;
+import com.example.demo.domain.products.entity.Product;
 import com.example.demo.domain.products.service.ProductsService;
-import com.example.demo.domain.products.service.response.ProductList;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/products")
-
+@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class ProductsController {
 
     final private ProductsService productsService;
-    public ProductsController(ProductsService productsService) {
-        this.productsService = productsService;
-    }
-
 
     @GetMapping(path = "/list")
-    public List<ProductList> productsList() {
+    public List<Product> productsList() {
         return productsService.list();
     }
 
+    @PostMapping(value = "/register", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public void productRegister (@RequestPart(value = "productImgList") List<MultipartFile> productImgList,
+                                 @RequestPart(value = "productInfo") ProductsRegisterForm form) {
+        log.info("productRegister()");
 
+        productsService.register(productImgList, form.toProductRegisterRequest());
+    }
 }
--- backend/src/main/java/com/example/demo/domain/products/service/ProductsServiceImpl.java
+++ backend/src/main/java/com/example/demo/domain/products/service/ProductsServiceImpl.java
@@ -1,15 +1,22 @@
 package com.example.demo.domain.products.service;
 
 import com.example.demo.domain.products.entity.Product;
+import com.example.demo.domain.products.entity.ProductImg;
+import com.example.demo.domain.products.repository.ProductsImgRepository;
 import com.example.demo.domain.products.repository.ProductsRepository;
-import com.example.demo.domain.products.service.response.ProductList;
+import com.example.demo.domain.products.service.request.ProductsRegisterRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -17,20 +24,48 @@ import java.util.List;
 public class ProductsServiceImpl implements ProductsService {
 
     final private ProductsRepository productsRepository;
+    final private ProductsImgRepository productsImgRepository;
 
     @Override
-    public List<ProductList> list() {
+    public List<Product> list() {
         List<Product> productList = productsRepository.findAll(Sort.by(Sort.Direction.DESC, "productId"));
-        List<ProductList> products = new ArrayList<>();
 
+        log.info("상품 리스트: " + String.valueOf(productList));
 
-        for (Product product: productList) {
-            products.add(new ProductList(
-                    product.getProductId(), product.getPrice(),
-                    product.getTitle(), product.getProductInfo()
-            ));
+        return productList;
+    }
+
+    @Override
+    public void register(List<MultipartFile> productImgList, ProductsRegisterRequest request) {
+        List<ProductImg> imgList = new ArrayList<>();
+        Product product = request.toProduct();
+
+        product.setTitle(request.getTitle());
+        product.setPrice(request.getPrice());
+        product.setContent(request.getContent());
+
+        final String fixedPath = "D:/sss/SSS-Front/frontend/src/assets/";
+        UUID uuid = UUID.randomUUID();
+
+        try {
+            for (MultipartFile multipartFile: productImgList) {
+                String name = uuid + multipartFile.getOriginalFilename();
+                FileOutputStream writer = new FileOutputStream((fixedPath + name));
+                writer.write(multipartFile.getBytes());
+                writer.close();
+
+                ProductImg productImg = new ProductImg(fixedPath + name);
+                productImg.setProduct(product);
+                imgList.add(productImg);
+                product.setProductImgs(imgList);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        return products;
+        productsRepository.save(product);
+        productsImgRepository.saveAll(imgList);
     }
 }
