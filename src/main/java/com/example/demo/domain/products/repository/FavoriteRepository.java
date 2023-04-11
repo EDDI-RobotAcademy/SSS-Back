f84fec8cebbe95f197166ed09963b965346b176d
dduru-nuna <syj00062@gmail.com>
2023-04-11 17:47:26 +0900

    feat: [서윤지] 회원이 상품 찜하기 요청시 찜 상태 저장하는 시스템[SSS-PROCESS-27]

---
+++ src/main/java/com/example/demo/domain/products/repository/FavoriteRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain.products.repository;
+
+import com.example.demo.domain.products.entity.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+    @Query("select f from Favorite f join fetch f.product fp join fetch f.member fm where fp.productId = :productId and fm.memberId = :memberId")
+    Optional<Favorite> findByProductAndMember(Long productId, Long memberId);
+
+    @Query("select f from Favorite f join fetch f.product fp join fetch f.member fm where fm.memberId = :memberId and f.isLike = true")
+    List<Favorite> findFavoriteByMemberId(Long memberId);
+}
