b29fd0cb4648248cf50eef553961caa744307c9a
dduru-nuna <syj00062@gmail.com>
2023-04-11 22:01:43 +0900

    test: [서윤지] 찜 요청 저장 테스트 [SSS-PROCESS-27]

---
+++ src/test/java/com/example/demo/product/favorite/ProductFavoriteTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.product.favorite;
+
+import com.example.demo.domain.products.service.FavoriteService;
+import com.example.demo.domain.products.service.request.FavoriteInfoRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductFavoriteTest {
+
+    @Autowired
+    FavoriteService favoriteService;
+
+    @Test
+    void 찜_저장() {
+        FavoriteInfoRequest request = new FavoriteInfoRequest(1L, 2L);
+        System.out.println(favoriteService.changeLike(request));
+    }
+
+    @Test
+    void 찜_상태() {
+        FavoriteInfoRequest request = new FavoriteInfoRequest(1L, 2L);
+        System.out.println(favoriteService.likeStatus(request));
+    }
+
+}
