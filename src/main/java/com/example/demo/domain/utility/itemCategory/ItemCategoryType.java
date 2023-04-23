c970ea3aec99c7a4edafb32bb79f72b392601bc1
hjlee5701 <hjlee5701@gmail.com>
2023-04-23 19:48:06 +0900

    move: [이현진] ItemCategory 파일을 utility 패키지로 이동

---
+++ src/main/java/com/example/demo/domain/utility/itemCategory/ItemCategoryType.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.utility.itemCategory;
+
+public enum ItemCategoryType {
+    // 데이터 전송할 때 SIDE_PRODUCT, PRODUCT, SELF 으로 전송하세요.
+
+    SIDE,
+    PRODUCT,
+    SELF;
+
+}
